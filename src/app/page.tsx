ebd9a1f722564e8bda8d9390e0b2ae844409271a
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-22 14:07:58 -0300

    feat: home new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Toast } from "../components/Toast";
+import { UploadImage } from "../components/UploadImage";
+
 export default function Home() {
   return (
     <>
-      <p>Hello word</p>
+      <Toast />
+      <main className="bg-background w-full h-screen flex flex-center justify-center">
+        <div className="w-full max-w-[420px] bg-white rouded-xl drop-shadow-md p-8">
+          <UploadImage />
+        </div>
+      </main>
     </>
   );
 }
