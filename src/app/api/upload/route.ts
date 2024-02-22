ebff1b02ae953cbb8b863509d9a5df02a0f00d8e
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-22 15:26:30 -0300

    feat: api

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,31 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+);
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file") as File;
+
+    const { data, error } = await supabase.storage
+      .from("imagens")
+      .upload(file.name, file);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    const { data: imageUrl } = supabase.storage
+      .from("images")
+      .getPublicUrl(data.path);
+
+    return Response.json({ imageUrl: imageUrl.publicUrl }, { status: 201 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({ error: error.message });
+    }
+  }
+}
